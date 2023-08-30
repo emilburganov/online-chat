2ff1dc6e99caaebe445f690cefad5f4896fd8699
Emil Burganov <emil.burganov03@gmail.com>
2023-08-30 22:00:52 +0300

    feat: system preference for light or dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React, {useContext} from 'react';
-import {BrowserRouter} from 'react-router-dom';
-import {CssBaseline} from '@mui/material';
-import {CssVarsProvider} from '@mui/joy/styles';
 import {Context} from '@/main.jsx';
+import {BrowserRouter} from 'react-router-dom';
+import {createTheme, CssBaseline, ThemeProvider, useMediaQuery} from '@mui/material';
+import {CssVarsProvider} from '@mui/joy/styles';
 import Header from '@/components/Header.jsx';
 import Loader from '@/components/UI/Loaders/Loader.jsx';
 import AppRouter from '@/router/AppRouter';
@@ -12,19 +12,32 @@ import '@/styles/App.css';
 function App() {
     const {auth} = useContext(Context);
     const [user, isLoading, error] = useAuthState(auth);
+    const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+
+    const theme = React.useMemo(
+        () =>
+            createTheme({
+                palette: {
+                    mode: prefersDarkMode ? 'dark' : 'light',
+                },
+            }),
+        [prefersDarkMode],
+    );
 
     return (
-        <CssVarsProvider disableTransitionOnChange>
-            <CssBaseline/>
-            {isLoading ?
-                <Loader/>
-                :
-                <BrowserRouter>
+        <ThemeProvider theme={theme}>
+            <CssVarsProvider disableTransitionOnChange>
+                <CssBaseline/>
+                {isLoading ?
+                    <Loader/>
+                    :
+                    <BrowserRouter>
                         <Header/>
                         <AppRouter/>
-                </BrowserRouter>
-            }
-        </CssVarsProvider>
+                    </BrowserRouter>
+                }
+            </CssVarsProvider>
+        </ThemeProvider>
     );
 }
 
