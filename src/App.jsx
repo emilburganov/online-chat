a4d510a3cafeac1acabf726a8fcc92090ce071da
Emil Burganov <emil.burganov03@gmail.com>
2023-08-30 22:05:08 +0300

    feat: system preference for light or dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, {useContext} from 'react';
 import {Context} from '@/main.jsx';
 import {BrowserRouter} from 'react-router-dom';
-import {createTheme, CssBaseline, ThemeProvider, useMediaQuery} from '@mui/material';
+import {CssBaseline} from '@mui/material';
 import {CssVarsProvider} from '@mui/joy/styles';
 import Header from '@/components/Header.jsx';
 import Loader from '@/components/UI/Loaders/Loader.jsx';
@@ -12,21 +12,9 @@ import '@/styles/App.css';
 function App() {
     const {auth} = useContext(Context);
     const [user, isLoading, error] = useAuthState(auth);
-    const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
-
-    const theme = React.useMemo(
-        () =>
-            createTheme({
-                palette: {
-                    mode: prefersDarkMode ? 'dark' : 'light',
-                },
-            }),
-        [prefersDarkMode],
-    );
 
     return (
-        <ThemeProvider theme={theme}>
-            <CssVarsProvider disableTransitionOnChange>
+            <CssVarsProvider defaultMode="system" disableTransitionOnChange>
                 <CssBaseline/>
                 {isLoading ?
                     <Loader/>
@@ -37,7 +25,6 @@ function App() {
                     </BrowserRouter>
                 }
             </CssVarsProvider>
-        </ThemeProvider>
     );
 }
 
